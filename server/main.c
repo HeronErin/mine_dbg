1dcf97bedde1419e438b81f44bdb62b2f0452c6f
HeronErin <HeronErin@protonmail.com>
2025-01-19 18:04:24 -0500

    ..

--- server/main.c
+++ server/main.c
@@ -1,23 +1,26 @@
-#include "proto_file.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "proto_file.h"
 
 #include "serde.h"
 
 
-int main(){
-	FILE *fp = fopen("/home/mitchell/Documents/mc_dbg/proto/packets/1.21.4.proto", "r");
+int main() {
+    FILE *fp = fopen("/home/mitchell/Documents/mc_dbg/proto/packets/1.21.4.proto", "r");
 
-	fseek(fp, 0, SEEK_END);
-	size_t len = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
+    fseek(fp, 0, SEEK_END);
+    size_t len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
 
-	char *buffer = malloc(len + 1);
-	fread(buffer, len, 1, fp);
-	buffer[len] = '\0';
-	fclose(fp);
-	PacketSerde_from_proto(buffer);
+    char *buffer = malloc(len + 1);
+    fread(buffer, len, 1, fp);
+    buffer[len] = '\0';
+    fclose(fp);
+    // PacketSerde_from_proto(buffer);
 
-	free(buffer);
-	return 0;
+    printf("%s\n", unescape_string("This is a test: \\X6a!"));
+
+
+    free(buffer);
+    return 0;
 }
